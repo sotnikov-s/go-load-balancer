f518ef5300c1dbda2e4c847691b97b0732e7cb19
Sergio Sotnikov <ssotnikov93@gmail.com>
2019-12-06 18:21:00 +0300

    move health related stuff to a distinct struct

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -4,73 +4,36 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"runtime"
-	"sync"
 	"time"
+
+	"github.com/sotnikov-s/go-load-balancer/pkg/proxy/health"
 )
 
 // NewProxy is the Proxy constructor
 func NewProxy(addr *url.URL) *Proxy {
 	return &Proxy{
-		origin:      addr,
-		proxy:       httputil.NewSingleHostReverseProxy(addr),
-		isAvailable: true,
+		proxy:  httputil.NewSingleHostReverseProxy(addr),
+		health: health.NewProxyHealth(addr),
 	}
 }
 
 // Proxy is a simple http proxy entity
 type Proxy struct {
-	origin *url.URL
+	health *health.ProxyHealth
 	proxy  *httputil.ReverseProxy
-
-	// TODO: move all health related stuff to distinct struct
-	healthMutex   *sync.RWMutex
-	healthChecker func(addr *url.URL) bool
-	healthCancel  chan struct{}
-	isAvailable   bool
-}
-
-func (p *Proxy) WithHealthCheck(checkFunc func(addr *url.URL) bool, period time.Duration) *Proxy {
-	if p.healthChecker != nil {
-		close(p.healthCancel)
-	}
-	p.healthMutex = &sync.RWMutex{}
-	p.healthChecker = checkFunc
-	p.healthCancel = make(chan struct{})
-	go p.runHealthCheck(period)
-
-	return p
-}
-
-func (p *Proxy) IsAvailable() bool {
-	p.healthMutex.RLock()
-	defer p.healthMutex.RUnlock()
-	return p.isAvailable
-}
-
-func (p *Proxy) runHealthCheck(period time.Duration) {
-	checkHealth := func() {
-		isAvailable := p.healthChecker(p.origin)
-		p.healthMutex.Lock()
-		defer p.healthMutex.Unlock()
-		p.isAvailable = isAvailable
-	}
-
-	t := time.NewTicker(period)
-	for {
-		select {
-		case <-t.C:
-			checkHealth()
-		case <-p.healthCancel:
-			t.Stop()
-			return
-		default:
-			runtime.Gosched()
-		}
-	}
 }
 
 // ServeHTTP proxies incoming requests
 func (p Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	p.proxy.ServeHTTP(w, r)
+}
+
+// IsAvailable returns whether the proxy origin was successfully connected at the last check time.
+func (p *Proxy) IsAvailable() bool {
+	return p.health.IsAvailable()
+}
+
+// SetHealthCheck sets the passed check func as the algorithm of checking the origin availability
+func (p *Proxy) SetHealthCheck(check func(addr *url.URL) bool, period time.Duration) {
+	p.health.SetHealthCheck(check, period)
 }
